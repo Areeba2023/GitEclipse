6be2a09589afe8d457a77bebddfb9bf05f55cf4d
Areeba2023 <areebahassan235@gmail.com>
2019-05-30 16:54:18 +0530

    Employee Details.

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,12 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Employee e1= new Employee();
+  e1.setEmpid(101);
+  e1.setEmpName("Areeba");
+  e1.setEmpSal(100000);
+	}
+
+}
