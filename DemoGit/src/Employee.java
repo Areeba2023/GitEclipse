f5e14352b1fc7efb36f6afe50facfcca53e1aa45
Areeba2023 <areebahassan235@gmail.com>
2019-05-30 16:44:15 +0530

    Employee Model

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,24 @@
+
+public class Employee {
+   private int empid;
+   private String empName;
+   private double empSal;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public double getEmpSal() {
+	return empSal;
+}
+public void setEmpSal(double empSal) {
+	this.empSal = empSal;
+}
+}
